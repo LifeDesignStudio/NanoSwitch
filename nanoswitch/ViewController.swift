09fd224fc670059609664b15c5f7aa3df599c0a7
Yoshi <ysyk0124@gmail.com>
2026-03-01 10:31:05 +0900

    Initial Commit

---
+++ nanoswitch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  nanoswitch
+//
+//  Created by Yoshiyuki Koyama on 2026/03/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ nanoswitchTests/nanoswitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  nanoswitchTests.swift
+//  nanoswitchTests
+//
+//  Created by Yoshiyuki Koyama on 2026/03/01.
+//
+
+import Testing
+@testable import nanoswitch
+
+struct nanoswitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
